928189dc9894ec10339e1252ba81476c8fe68571
Jacob Yoo <jacobyoo@gmail.com>
2017-10-20 14:13:20 -0700

    basic recipes component layout

--- client/components/recipes.js
+++ client/components/recipes.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
+import SearchBar from './search-bar.js';
+import RecipeEntry from './recipe-entry.js';
+
 class Recipes extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
-      <div>
-        Hello Recipes!
+      <div className="container">
+        <h4 className="component-title">Recipes</h4>
+        <div>
+          <SearchBar />
+          <strong>Trending Keywords:</strong> <a href="#">Ramen</a>, <a href="#">Ramen</a>, <a href="#">Ramen</a>, <a href="#">Ramen</a>
+        </div>
+        <div align="right">
+          <a className="waves-effect waves-light btn"><i class="material-icons left">local_dining</i>Add Your Own!</a>
+        </div>
+        <div className="recipe-entries">
+          <RecipeEntry />
+          <RecipeEntry />
+          <RecipeEntry />
+          <RecipeEntry />
+          <RecipeEntry />
+        </div>
       </div>
     );
   }
