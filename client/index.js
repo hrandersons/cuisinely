dab6519115a2c56584d618bfab02f5fca7158243
Bois Bakhodirov <bois.bb18@gmail.com>
2017-10-19 21:45:10 -0400

    Set up Redux

--- client/index.js
+++ client/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/app.js';
+import { Provider } from 'react-redux';
+import store from './store';
 
-ReactDOM.render((
+ReactDOM.render((<Provider store={store}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
+  </Provider>
   ), document.getElementById('app'));
---
+++ client/store.js
@@ -0,0 +1,13 @@
+import {applyMiddleware, createStore} from 'redux';
+import promise from 'redux-promise-middleware';
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import reducer from './reducers/mainReducer';
+
+const logger = createLogger({});
+const middleware = applyMiddleware(promise(), thunk, logger);
+
+export default  createStore(
+  reducer,
+  middleware
+)
