c41ed80b07428486af7ba1cfc9a44caa4d6a058f
Jacob Yoo <jacobyoo@gmail.com>
2017-10-19 17:40:06 -0700

    implemented basic routing for major components and added additional components

---
+++ client/components/dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Dashboard extends React.Component {
+  render() {
+    return(
+      <div>
+        Hello Dashboard!
+      </div>
+    )
+  }
+}
+
+export default Dashboard;
---
+++ client/components/user-profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class UserProfile extends React.Component {
+  render() {
+    return(
+      <div className="user-profile">
+        <h3>User Name</h3>
+        <div>and then some user info...</div>
+      </div>
+    )
+  }
+}
+
+export default UserProfile;
