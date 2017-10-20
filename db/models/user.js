11ab2f8f321bd3ad368df4560e5d1a17b12276f1
Lillian Lee <lillianlee118@gmail.com>
2017-10-20 15:16:12 -0700

    user schema added

---
+++ db/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const ifeedme = require('../config');
+const Promise = require('bluebird');
+
+userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    userid: { type: String },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+  }
+);
+
+var User = mongoose.model('User', userSchema);
+module.exports = User;
+
+//fields always generated with Auth0
+//name
+//nickname
+//picture
+//user id
+
+//fields generated when details available
+//email
+//email_verified (boolean indicating verified or not)
+//given name
+//family name
