bc99a2b19456e14bb0afb8c0d51adbad8250f117
Lillian Lee <lillianlee118@gmail.com>
2017-10-20 19:02:40 -0700

    prelim layout of recipe page

--- client/components/recipe-details.js
+++ client/components/recipe-details.js
@@ -17,6 +17,7 @@ class RecipeDetails extends React.Component {
 
   render() {
     return (
+<<<<<<< HEAD
       <div className="row">
         <div className="col s12 m10">
           <div className="card">
@@ -106,6 +107,53 @@ class RecipeDetails extends React.Component {
         </div>
       </div>
     );
+=======
+      <Card>
+        <CardMedia
+        >
+          <img src="https://search.chow.com/thumbnail/1280/800/www.chowstatic.com/assets/2014/11/31178_slow_cooker_pork_ramen_3000.jpg" alt="" />
+        </CardMedia>
+        <CardTitle title="Ramen" subtitle="Difficulty: Very Easy, Estimated Time: 25 min" />
+        <CardText>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHeaderColumn>Ingredients</TableHeaderColumn>
+                <TableHeaderColumn>Amount</TableHeaderColumn>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableRowColumn>Ramen Noodles</TableRowColumn>
+                <TableRowColumn>1 pack</TableRowColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn>Egg</TableRowColumn>
+                <TableRowColumn>1</TableRowColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn>Chicken Broth</TableRowColumn>
+                <TableRowColumn>4 cups</TableRowColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn>Pork</TableRowColumn>
+                <TableRowColumn>1/2 pound</TableRowColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn>Scallions</TableRowColumn>
+                <TableRowColumn>1 stock</TableRowColumn>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </CardText>
+        <CardActions>
+          <FlatButton label="Print" />
+          <FlatButton label="Email" />
+        </CardActions>
+      </Card>
+    );
+
+>>>>>>> prelim layout of recipe page
   }
 }
 
