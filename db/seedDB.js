f360a2fe47f1d59c1bc6c280ef255b3cf75541da
Douglas Salazar <countertenor.salazar@gmail.com>
2017-10-21 17:59:13 -0400

    Wrote data formatting functions, formatted sample recipe data, wrote seedDB function.

---
+++ db/seedDB.js
@@ -0,0 +1,35 @@
+let recipeData = require('./recipes.json');
+const Recipe = require('./models/recipe.js');
+const seedData = JSON.parse(recipeData);
+
+let seedDB = () => {
+  seedData.forEach((recipe) => {
+    Recipe.findOne({ name: recipe.name }, (err, entry) => {
+      if (err) {
+        console.log(err);
+      } else if (entry) {
+        console.log('Recipe already exists!');
+        return;
+      } else {
+        let newRecipe = new Recipe({
+          name: recipe.name,
+          ingredients: recipe.ingredients,
+          time: recipe.time,
+          instructions: recipe.instructions,
+          difficulty: recipe.difficulty,
+          rating: recipe.rating,
+          imageUrl: recipe.imageUrl,
+          source: recipe.source
+        });
+        newRecipe.save((err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log('Recipe saved!');
+          }
+        });
+      }
+    });
+  });
+  console.log('Database seeded!');
+};
