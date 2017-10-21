fab104d69264f6bbacf00c81bdeb986e9aff55f7
Jacob Yoo <jacobyoo@gmail.com>
2017-10-20 21:13:22 -0700

    refactored and removed jQuery

--- client/components/nav-bar.js
+++ client/components/nav-bar.js
@@ -1,26 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { SideNav, SideNavItem, Button } from 'react-materialize';
+
 import SideDrawer from './side-drawer.js';
 
 class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
 
-  componentDidMount() {
-    $('.button-collapse').sideNav({ closeOnClick: true });
-    $('.button-profile').sideNav({
-      edge: 'right',
-      closeOnClick: true
-    });
+    this.handleLogOut = this.handleLogOut.bind(this);
   }
+
+  handleLogOut() {
+    console.log('logging out');
+  }
+
   render() {
     return (
       <div className="nav-bar">
         <nav>
           <div className="nav-wrapper teal lighten-2">
             <span className="brand-logo center nav-bar-title">ifeedme</span>
-            <a data-activates="mobile" className="button-collapse">
-              <i className="material-icons">menu</i>
-            </a>
+            <SideNav
+              trigger={<a className="button-collapse">
+                <i className="material-icons">menu</i>
+              </a>}
+              options={{ closeOnClick: true }}
+            >
+              <div align="center">
+                <Button className="waves-effect waves-light btn red lighten-1" onClick={this.handleLogOut}>Log Out</Button>
+              </div>
+              <ul>
+                <li><Link to="dashboard">Dashboard</Link></li>
+                <li><Link to="recipes">Recipes</Link></li>
+                <li><Link to="meals">Meals</Link></li>
+              </ul>
+            </SideNav>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
               <li>
                 <Link to="dashboard">Dashboard</Link>
@@ -32,28 +48,21 @@ class NavBar extends React.Component {
                 <Link to="meals">Meals</Link>
               </li>
               <li>
-                <a data-activates="profile" className="button-profile">
-                  <i className="material-icons">person</i>
-                </a>
+                <SideNav
+                  trigger={<a className="button-profile">
+                    <i className="material-icons">person</i>
+                  </a>}
+                  options={{ closeOnClick: true, edge: 'right' }}
+                >
+                  <div>
+                    <SideDrawer />
+                  </div>
+                </SideNav>
               </li>
             </ul>
-            <ul className="side-nav" id="profile">
-              <li><SideDrawer/></li>
-            </ul>
-            <ul className="side-nav" id="mobile">
-              <li>
-                <a className="waves-effect waves-light btn red lighten-1" onClick={this.handleLogOut}>Log Out</a>
-              </li>
-              <li>
-                <Link to="dashboard">Dashboard</Link>
-              </li>
-              <li>
-                <Link to="recipes">Recipes</Link>
-              </li>
-              <li>
-                <Link to="meals">Meals</Link>
-              </li>
-            </ul>
+
+
+
 
           </div>
         </nav>
