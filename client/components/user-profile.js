d37a166f25615327d1efff25733eac9243a8e915
Lillian Lee <lillianlee118@gmail.com>
2017-10-23 12:15:07 -0700

    started implementing profile

--- client/components/user-profile.js
+++ client/components/user-profile.js
@@ -1,11 +1,37 @@
 import React from 'react';
 
 class UserProfile extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: null,
+      picture: null,
+    };
+    console.log(localStorage.profile);
+    // this.getProfile = this.getProfile.bind(this);
+  }
+
+  componentWillMount() {
+    this.setState({
+      name: localStorage.profile.name,
+      picture: localStorage.profile.picture,
+    });
+  }
+
+  // getProfile() {
+  //   this.setState({
+  //     name: localStorage.profile.name,
+  //     picture: localStorage.profile.picture,
+  //
+  //   })
+  // }
+
   render() {
     return (
       <div className="user-profile container">
         <div>
-          <strong>User Name</strong>
+          <strong>Name: {this.state.name}</strong>
         </div>
         <div>
           <img className="responsive-img circle" src="http://lorempixel.com/150/150/people/"/>
