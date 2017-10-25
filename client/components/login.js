8d2c45666c6d0a1213426f4b089751ffdbb2ea64
Lillian Lee <lillianlee118@gmail.com>
2017-10-25 12:44:52 -0700

    changes to login page

--- client/components/login.js
+++ client/components/login.js
@@ -8,31 +8,27 @@ class Login extends React.Component {
   login() {
     this.props.auth.login();
   }
+
   render() {
-    const { isAuthenticated } = this.props.auth;
     return (
-      <div className="container">
-        {
-          isAuthenticated() && (
-            <h4>
-              Welcome to ifeedme!
-            </h4>
-          )
-        }
-        {
-          !isAuthenticated() && (
-            <h4>
-              Welcome to ifeedme! Please{' '}
-              <a
-                style={{ cursor: 'pointer' }}
-                onClick={this.login.bind(this)}
-              >
-                Log In
-              </a>
-              {' '}to continue.
-            </h4>
-          )
-        }
+      <div className="login-container">
+        <div className="row">
+          <div className="col s12">
+            <div className="card white">
+              <div className="card-content blue-text">
+                <span className="card-title landing-title">Welcome to ifeedme!</span>
+              </div>
+              <div className="card-action landing">
+                <strong><a
+                  style={{ cursor: 'pointer' }}
+                  onClick={this.login.bind(this)}
+                >
+                  Log In or Sign Up
+                </a></strong>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
