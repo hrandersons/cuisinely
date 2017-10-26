3ea6c1e1e7665614c348c4726a4912d602e17468
Bois Bakhodirov <bois.bb18@gmail.com>
2017-10-26 00:09:06 -0400

    Adding Auth keys

--- client/Auth/Auth.js
+++ client/Auth/Auth.js
@@ -2,7 +2,7 @@ import Auth0Lock from 'auth0-lock';
 import keys from './Auth_keys';
 import history from '../components/history';
 
-const lock = new Auth0Lock(keys.clientId, keys.domain, {
+const lock = new Auth0Lock('sNfZXyIkcjg3QZve68HJXoGfzFVZgjE4', 'ifeedme.auth0.com', {
   oidcConformant: false,
   autoclose: true,
   redirect: true,
