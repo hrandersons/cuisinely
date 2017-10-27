91445e1ad616c9deb61a3a56de2d109e54ca1f62
Bois Bakhodirov <bois.bb18@gmail.com>
2017-10-27 15:03:51 -0400

    Travis should work

--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-set -x
-tar -czf package.tgz build && \
-scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
-ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
