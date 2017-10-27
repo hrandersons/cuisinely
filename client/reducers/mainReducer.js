fea8ccd6538e439947812435245e8fd1f687925a
Douglas Salazar <countertenor.salazar@gmail.com>
2017-10-26 22:55:23 -0400

    Now setting user info in redux state on login.

--- client/reducers/mainReducer.js
+++ client/reducers/mainReducer.js
@@ -1,15 +1,20 @@
-import { SET_SHOPPING_LIST } from '../actions/actions.js';
+import { SET_SHOPPING_LIST, SET_USER_INFO } from '../actions/actions.js';
 const initialState = {
   loggedIn: '',
-  shoppingList: []
+  shoppingList: [],
+  user: {}
 };
 
 export default function(state = initialState, action) {
   let shoppingList;
+  let user;
   switch (action.type) {
   case SET_SHOPPING_LIST:
     shoppingList = action.payload;
     return {...state, shoppingList };
+  case SET_USER_INFO:
+    user = action.payload;
+    return {...state, user};
   default:
     return state;
   }
