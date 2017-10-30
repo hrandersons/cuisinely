76359d306849685ab03136d72b6372f40c4e985d
Bois Bakhodirov <bois.bb18@gmail.com>
2017-10-30 15:10:14 -0400

    Travis check

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,16 +2,12 @@
 set -x
 if [ $TRAVIS_BRANCH == 'develop' ] ; then
     # Initialize a new git repo in _site, and push it to our server.
-    mkdir _site && cd _site
-    git init
-        
     git remote add deploy "deploy@repo:/var/repo"
     git config user.name "boisbb18"
     git config user.email "bois.bb18@gmail.com"
     
     git add .
     git commit -m "Deploy"
-    git push origin master
     git push --force deploy master
 else
     echo "Not deploying, since this branch isn't master."
