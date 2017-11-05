2a5e6294f7cc733793627b75ea7f5fb8600500ed
Bois Bakhodirov <bois.bb18@gmail.com>
2017-11-05 12:41:12 -0500

    Removed uglify from webpack.prod

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -13,12 +13,6 @@ module.exports = {
     publicPath: '/dist/'
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      compress: {
-        warnings: false
-      }
-    }),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
