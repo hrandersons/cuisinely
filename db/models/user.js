377c0b79f127069d975b1a973c0c3eb38cadefcd
Lillian Lee <lillianlee118@gmail.com>
2017-11-08 16:44:03 -0800

    change

--- db/models/user.js
+++ db/models/user.js
@@ -3,12 +3,12 @@ const ifeedme = require('../config');
 mongoose.Promise = require('bluebird');
 
 userSchema = new mongoose.Schema({
-    userId: { type: String, required: true, index: { unique: true }},
-    bookmarks: { type: [] },
-    points: {type: Number },
-    pointsGraph: { type: []},
-    level: { type: Number },
-    weeklyPoints: { type: {} }
+  userId: { type: String, required: true, index: { unique: true }},
+  bookmarks: { type: [] },
+  points: {type: Number },
+  pointsGraph: { type: []},
+  level: { type: Number },
+  weeklyPoints: { type: {} }
 });
 
 var User = mongoose.model('User', userSchema);
